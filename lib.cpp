ccc0862da8f42605b00092dc361cfae37f7c2bc9
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-11-30 11:31:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by tomma on 29/11/2021.
+//
 
+#include "lib.h"
+
+int funzione(int a){
+    int  divisori=1, divisori_interi=0;
+
+    while(divisori_interi<=1 && divisori<=a/2) {
+        if(a%divisori==0){
+            divisori_interi++;
+        }divisori++;}
+
+    if (divisori_interi==1)
+    {
+        return 1;
+    } else
+    {
+        return 0;
+    }}
