dd7fc37dfefc67f6d0fa6a52c2341c7a1bbb56dc
TommasoBolioli <91489369+TommasoBolioli@users.noreply.github.com>
2021-11-30 11:30:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std;
+#include "lib.h"
 
-int main(){
-  return 0;
+int main() {
+    int a=0, ris;
+    cin >> a;
+    ris= funzione(a);
+    if (ris==1){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
